4a8dfa3e1cd2a3cd84aaba08103a5c4442cd8479
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-07 21:06:04 +0200

    es1-commit

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,20 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+
+// Dichiara la funzione qui.
+
+function findInitials(names) {
+    return names.map(name => name.charAt(0));
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+const initials = findInitials(names);
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+console.log(initials);
