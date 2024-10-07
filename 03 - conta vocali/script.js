66c1f606decb11f4a6e641ab455703da945498d5
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-07 21:31:59 +0200

    es3-commit

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function CountVowels(str) {
+    let count= 0;
+ for (let i = 0; i<str.lenght; i++){
+    let char == str[i].toLowercase();
+    if (char == 'a'|| char == 'e' || char == 'i'|| char == 'o'|| char == 'u'||) {
+        count++;
+    }
+ }
 
-
+ return count;
+ }
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const word = 'javascript';
+console.log(CountVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
