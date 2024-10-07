da8674ac9e2048d24aaa29b6b19557014717831f
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-07 21:39:48 +0200

    es3-adjustments-commit

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,17 +1,17 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+//const word = 'javascript';
 
 
 // Dichiara la funzione qui.
 function CountVowels(str) {
     let count= 0;
- for (let i = 0; i<str.lenght; i++){
-    let char == str[i].toLowercase();
-    if (char == 'a'|| char == 'e' || char == 'i'|| char == 'o'|| char == 'u'||) {
+ for (let i = 0; i<str.length; i++){
+    let char = str[i].toLowerCase();
+    if (char === 'a'|| char === 'e' || char === 'i'|| char === 'o'|| char === 'u')
         count++;
     }
- }
+ 
 
  return count;
  }
