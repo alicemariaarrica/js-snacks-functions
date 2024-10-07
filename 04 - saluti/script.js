0a421383aa8a7d38a58d23fed0d8cc19ff359b97
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-07 21:45:48 +0200

    es4-commit

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+//const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+ function Greet(name) {
+    return 'ciao' + name;
+ }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const name = 'Mario';
+console.log(Greet( name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
